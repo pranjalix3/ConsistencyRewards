57f2b9e107fcf752ff103f50817ebfff6e932ace
Pranjali <srivastavapranjali603@gmail.com>
2025-11-02 19:59:22 +0530

    Full Stack Project Setup

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  protected readonly title = signal('consistency-rewards-ui');
+  backendMessage = signal('');
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/api/test', { responseType: 'text' })
+      .subscribe(response => {
+        this.backendMessage.set(response);
+      });
+  }
+}
